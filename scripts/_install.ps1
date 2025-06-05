210527cbcffb5bb66221f805d26515bb6f45741f
Oscar-Kristensson <kristenssonomg@gmail.com>
2025-06-05 15:39:39 +0200

    Added an icon to the executable

--- scripts/_install.ps1
+++ scripts/_install.ps1
@@ -1,9 +1,9 @@
 # === Configuration ===
-$ShortcutName = "BoardGame"                      # Name of the shortcut
-$TargetPath = "launcher.vbs"                    # Full path to the executable
-#$IconLocation = "C:\Path\To\MyApp.ico"          # (Optional) Path to .ico file
-$Arguments = ""                                  # (Optional) Command-line arguments
-# $WorkingDirectory = "C:\Path\To"                # (Optional) Start-in directory
+$ShortcutName = "BoardGame"                         # Name of the shortcut
+$TargetPath = "launcher.vbs"                        # Full path to the executable
+$IconLocation = "assets/icons/icon.ico"             # (Optional) Path to .ico file
+$Arguments = ""                                     # (Optional) Command-line arguments
+# $WorkingDirectory = "C:\Path\To"                  # (Optional) Start-in directory
 
 # === Create Shortcut ===
 $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
@@ -16,7 +16,7 @@ $Shortcut = $Shell.CreateShortcut($StartMenuPath)
 $Shortcut.TargetPath = Join-Path $ScriptDir $TargetPath
 $Shortcut.Arguments = $Arguments
 $Shortcut.WorkingDirectory = $ScriptDir
-#$Shortcut.IconLocation = $IconLocation
+$Shortcut.IconLocation = Join-Path $ScriptDir $IconLocation
 $Shortcut.Save()
 
 Write-Host "Shortcut created at: $StartMenuPath"
